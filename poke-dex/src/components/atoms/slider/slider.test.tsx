380be78cbad507a4476adebbd3c08b96e4d4a4bf
Carlos Alexander Puco Logroño <44406691+carlospuco@users.noreply.github.com>
2023-03-09 15:50:38 -0500

    delete components extra

--- poke-dex/src/components/atoms/slider/slider.test.tsx
+++ poke-dex/src/components/atoms/slider/slider.test.tsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import { render, fireEvent,screen, getByTestId } from '@testing-library/react';
-import Slider from './slider';
+import React from "react";
+import { render, fireEvent, screen, getByTestId } from "@testing-library/react";
+import Slider from "./slider";
 
-describe('Slider', () => {
-  test('renders correctly', () => {
-     render(
-      <Slider value={50} onChange={() => {}} />
-    );
-    const inputElement = screen.getByTestId('slider');
-    const minTextElement = screen.getByText('0');
-    const maxTextElement = screen.getByText('50');
+describe("Slider", () => {
+  test("renders correctly", () => {
+    render(<Slider value={50} onChange={() => {}} />);
+    const inputElement = screen.getByTestId("slider");
+    const minTextElement = screen.getByText("0");
+    const maxTextElement = screen.getByText("50");
 
     expect(inputElement).toBeInTheDocument();
-    expect(inputElement).toHaveAttribute('type', 'range');
-    expect(inputElement).toHaveAttribute('min', '0');
-    expect(inputElement).toHaveAttribute('max', '100');
-    expect(inputElement).toHaveAttribute('value', '50');
+    expect(inputElement).toHaveAttribute("type", "range");
+    expect(inputElement).toHaveAttribute("min", "0");
+    expect(inputElement).toHaveAttribute("max", "100");
+    expect(inputElement).toHaveAttribute("value", "50");
     expect(minTextElement).toBeInTheDocument();
     expect(maxTextElement).toBeInTheDocument();
   });
 
-  test('calls onChange correctly', () => {
+  test("calls onChange correctly", () => {
     const handleChange = jest.fn();
-    render(
-      <Slider value={50} onChange={handleChange} />
-    );
-    const inputElement = screen.getByTestId('slider');
+    render(<Slider value={50} onChange={handleChange} />);
+    const inputElement = screen.getByTestId("slider");
 
     fireEvent.change(inputElement, { target: { value: 75 } });
 
     expect(handleChange).toHaveBeenCalledTimes(1);
     expect(handleChange).toHaveBeenCalledWith(75);
+  });
+
+  test("does not change value when disabled", () => {
+    render(<Slider value={50} onChange={() => {}} />);
+    const inputElement = screen.getByTestId("slider");
+
+    fireEvent.change(inputElement, { target: { value: 75 } });
+
+    expect(inputElement).toHaveAttribute("value", "50");
+  });
+
+  test("handles input of non-numeric value", () => {
+    const handleChange = jest.fn();
+    render(<Slider value={50} onChange={handleChange} />);
+    const inputElement = screen.getByTestId("slider");
+
+    fireEvent.change(inputElement, { target: { value: "abc" } });
+
+    expect(inputElement).toHaveAttribute("value", "50");
+    expect(handleChange).toHaveBeenCalledTimes(0);
   });
 });
--- poke-dex/src/components/molecules/table/table.tsx
+++ poke-dex/src/components/molecules/table/table.tsx
@@ -2,10 +2,10 @@ import React, { FC } from "react";
 import Button from "../../atoms/button/button";
 import Label from "../../atoms/label/label";
 import "./table.scss";
-import DeleteIcon from "../../../assets/delete.svg"
-import EditIcon from "../../../assets/edit.svg"
+import DeleteIcon from "../../../assets/delete.svg";
+import EditIcon from "../../../assets/edit.svg";
 
-interface PokemonType{
+interface PokemonType {
   nombre: string;
   imagen: string;
   ataque: number;
@@ -13,38 +13,46 @@ interface PokemonType{
 }
 
 interface TableProps {
-  pokemons: PokemonType[],
+  pokemons: PokemonType[];
 }
 
-const Table: FC<TableProps> = ({
-  pokemons
-}) => {
+const Table: FC<TableProps> = ({ pokemons }) => {
   return (
     <div className="table">
       <table>
         <tr>
-          <th><Label change="style">Nombre</Label></th>
-          <th><Label change="style">Imagen</Label></th>
-          <th><Label change="style">Ataque</Label></th>
-          <th><Label change="style">Defensa</Label></th>
-          <th><Label change="style">Acciones</Label></th>
+          <th>
+            <Label change="style">Nombre</Label>
+          </th>
+          <th>
+            <Label change="style">Imagen</Label>
+          </th>
+          <th>
+            <Label change="style">Ataque</Label>
+          </th>
+          <th>
+            <Label change="style">Defensa</Label>
+          </th>
+          <th>
+            <Label change="style">Acciones</Label>
+          </th>
         </tr>
-        {
-          pokemons?.map(pokemon => (
-            <tr>
-              <td>{pokemon.nombre}</td>
-              <td><img src={pokemon.imagen} alt="new pokemon image" width="90px"/></td>
-              <td>{`${pokemon.ataque}`}</td>
-              <td>{`${pokemon.defensa}`}</td>
-              <td>{
-                <div className="table__tableIcon">
-                <Button action="icon-actions"  picture={EditIcon}></Button>
-                <Button action="icon-actions"  picture={DeleteIcon}></Button>
-                </div>
-                }</td>
-            </tr>
-          ))
-        }
+        {pokemons?.map((pokemon) => (
+          <tr>
+            <td>{pokemon.nombre}</td>
+            <td>
+              <img src={pokemon.imagen} alt="new pokemon image" width="90px" />
+            </td>
+            <td>{`${pokemon.ataque}`}</td>
+            <td>{`${pokemon.defensa}`}</td>
+            <td>
+              <div className="table__tableIcon">
+                <Button action="icon-actions" picture={EditIcon}></Button>
+                <Button action="icon-actions" picture={DeleteIcon}></Button>
+              </div>
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );
